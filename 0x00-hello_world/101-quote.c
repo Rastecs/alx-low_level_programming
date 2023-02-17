981d0328891bcb32fa4ff46f6f233ecc65ae512b
Rastecs <rastecsisanda@gmail.com>
2023-02-17 06:04:54 -0800

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Prints a sentence
+ *
+ * Returns: 1
+ */
+int main (void)
+{
+	char sent[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, sent, 59);
+	return (1);
+}
