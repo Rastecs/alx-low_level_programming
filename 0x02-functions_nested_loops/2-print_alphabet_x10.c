b5ef25f74ff87d6642911af05195f816f8e1063c
Rastecs <rastecsisanda@gmail.com>
2023-02-22 12:25:11 -0800

    displays alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10(void) - prints alphabet 10x in lower
+ *
+ * Return: none(void)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			putchar(j);
+		}
+	}
+	putchar('\n');
+}
