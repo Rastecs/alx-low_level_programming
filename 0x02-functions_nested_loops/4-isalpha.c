d6a3245bded4f35c092bc8e05e3e2231b9a95ef8
Rastecs <rastecsisanda@gmail.com>
2023-02-22 13:08:46 -0800

    checks for alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks if alpha
+ *
+ * @c: ASCII character
+ *
+ * Return: 1 if true and 0 if false
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
