e22cf0ccdddb8d66cd0ec1064a6eeb6f92e877f8
Rastecs <rastecsisanda@gmail.com>
2023-03-01 07:47:09 -0800

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,14 @@
-#include <stdi0.h>
-
+#include <stdio.h>
+/*
+ * main - Add one line to this code, so that the program prints a[2] = 98
+ * Return: o
+ */
 int main(void)
-{ 
+{
 	int n;
 	int a[5];
 	int *p;
-	
+
 	a[2] = 1024;
 	p = &n;
 	/*
